433adbd2b0f49a08ff203484bb7e032bbe0970a0
Elijah Nathan <ely.nathan93@gmail.com>
2018-07-21 00:19:42 +0100

    Add basic tab functionality

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -1,1 +1,31 @@
-console.log('JS compiling');
+window.addEventListener('load', function() {
+  let theTabs = document.querySelectorAll('[data-target]');
+  let i;
+
+  function theTabClicks(tabClickEvent) {
+    let clickedTab = tabClickEvent.currentTarget;
+    let tabParent =
+    tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+    let theTabs = tabParent.querySelectorAll('[data-target]');
+    for (i = 0; i < theTabs.length; i++) {
+      theTabs[i].classList.remove('active');
+    }
+
+    clickedTab.classList.add('active');
+    tabClickEvent.preventDefault();
+    let contentPanes = tabParent.querySelectorAll('[data-id]');
+    for (i = 0; i < contentPanes.length; i++) {
+      contentPanes[i].classList.remove('active');
+    }
+    let anchorReference = tabClickEvent.target;
+    let activePaneId = anchorReference.getAttribute('data-target');
+    // console.log(activePaneId + " | " + activePane);
+    let activePane =
+    tabParent.querySelectorAll('[data-id="' + activePaneId + '"]');
+    activePane[0].classList.add('active');
+  }
+
+  for (let i = 0; i < theTabs.length; i++) {
+    theTabs[i].addEventListener('click', theTabClicks)
+  }
+});
